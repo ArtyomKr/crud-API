d519f4ce78aa25e747a96a88995be46579bf72cd
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-26 22:19:07 +0300

    fix: change user type guard argument type to 'unknown'

--- src/utils/userTypeGuard.ts
+++ src/utils/userTypeGuard.ts
@@ -1,7 +1,13 @@
 import { IUser } from './models.js';
 
-export default function isUserObj(obj: any): obj is Omit<IUser, 'id'> {
-  if ('username' in obj && 'age' in obj && 'hobbies' in obj) {
+export default function isUserObj(obj: unknown): obj is Omit<IUser, 'id'> {
+  if (
+    typeof obj === 'object' &&
+    obj &&
+    'username' in obj &&
+    'age' in obj &&
+    'hobbies' in obj
+  ) {
     return (
       typeof obj['username'] === 'string' &&
       typeof obj['age'] === 'number' &&
