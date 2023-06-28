ff01d5d5fa71e24a911a50dbfd3e93ebc93ac39e
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-28 23:53:50 +0300

    fix: fix unreachable error handling

--- src/endpoints/errorHandler.ts
+++ src/endpoints/errorHandler.ts
@@ -3,5 +3,5 @@ import { ServerResponse } from 'http';
 export default function handleError(res: ServerResponse, err: unknown) {
   res.setHeader('Content-Type', 'text/html');
   res.statusCode = 500;
-  res.write((<Error>err).message ?? 'Unknown error occurred');
+  res.end((<Error>err).message ?? 'Unknown error occurred');
 }
