addfd611a60156577c8a32dc1fdb542d1ce5b998
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-28 23:56:39 +0300

    fix: add single state of truth for workers

--- src/app-multi.ts
+++ src/app-multi.ts
@@ -4,31 +4,48 @@ import 'dotenv/config';
 import { createServer } from 'http';
 import requestListener from './handlers/requestListener.js';
 import createLoadBalancer from './handlers/loadBalancer.js';
+import { getUsersDB, setUsersDB } from './db/usersDB.js';
+import { IClusterMessage } from './utils/models';
 
 const port = process.env.PORT || 3000;
-const origin = process.env.ORIGIN || "http://localhost";
+const origin = process.env.ORIGIN || 'http://localhost';
 
 const numCPUs = os.cpus().length;
 
 if (cluster.isPrimary) {
-  const workerAdressPool = [];
+  const workerAddressPool = [];
 
   for (let i = 1; i < numCPUs; i++) {
     const workerPort = +port + i;
-    workerAdressPool.push(`${origin}:${workerPort}`);
+    workerAddressPool.push(`${origin}:${workerPort}`);
 
-    cluster.fork({ PORT: workerPort });
+    const worker = cluster.fork({ PORT: workerPort });
+
+    worker.on('message', (message: IClusterMessage) => {
+      if (message.action === 'updateDB' && message.payload)
+        setUsersDB(message.payload);
+      if (message.action === 'sendDB')
+        worker.send({ action: 'receiveDB', payload: getUsersDB() });
+    });
   }
-
-  const loadBalancer = createServer(createLoadBalancer(workerAdressPool));
+  const loadBalancer = createServer(createLoadBalancer(workerAddressPool));
 
   loadBalancer.listen(port, () => {
-    console.log(`Load balancer running at ${origin}:${port}/`);
+    console.log(
+      '\x1b[33m%s\x1b[0m',
+      `Load balancer running at ${origin}:${port}/`
+    );
   });
 } else {
   const server = createServer(requestListener);
 
   server.listen(port, () => {
     console.log(`Worker running at ${origin}:${port}/`);
+  });
+
+  process.on('message', (message: IClusterMessage) => {
+    if (message.action === 'syncDB' && message.payload) {
+      setUsersDB(message.payload);
+    }
   });
 }
--- src/db/usersDB.ts
+++ src/db/usersDB.ts
@@ -1,15 +1,28 @@
-import { IDb, IUser } from '../utils/models';
+import { IClusterMessage, IDb, IUser } from '../utils/models';
+import cluster from 'cluster';
 
 const db: IDb = {
   users: []
 };
 
 function setUsersDB(arr: IUser[]) {
-  db.users = arr;
+  if (cluster.isWorker && process.send) {
+    process.send({ action: 'updateDB', payload: arr });
+  } else db.users = arr;
   return db.users;
 }
 
 function getUsersDB() {
+  if (cluster.isWorker && process.send) {
+    return new Promise<IUser[]>((res) => {
+      process.send?.({ action: 'sendDB' });
+      process.on('message', (message: IClusterMessage) => {
+        if (message.action === 'receiveDB' && message.payload) {
+          res(message.payload);
+        }
+      });
+    });
+  }
   return db.users.slice();
 }
 
