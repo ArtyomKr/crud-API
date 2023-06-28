dd1b0539e0805ab2f359c4adf9fc3af3ed52b32b
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-28 23:55:46 +0300

    fix: add body detection to read body

--- src/utils/readReqBody.ts
+++ src/utils/readReqBody.ts
@@ -1,9 +1,13 @@
 import { IncomingMessage } from 'http';
 
 export default async function readReqBody(req: IncomingMessage) {
-  const buffers = [];
-  for await (const chunk of req) {
-    buffers.push(chunk);
-  }
-  return JSON.parse(Buffer.concat(buffers).toString());
+  const contentLength = req.headers['content-length'];
+  if (contentLength && +contentLength > 0) {
+    const buffers = [];
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+
+    return JSON.parse(Buffer.concat(buffers).toString() ?? '');
+  } else return null;
 }
