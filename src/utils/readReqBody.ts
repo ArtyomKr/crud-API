b6d8f2905b1e09c87cf4f32a2b6063513764fadf
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-29 10:56:26 +0300

    fix: fix test erros

--- src/utils/readReqBody.ts
+++ src/utils/readReqBody.ts
@@ -1,13 +1,10 @@
 import { IncomingMessage } from 'http';
 
 export default async function readReqBody(req: IncomingMessage) {
-  const contentLength = req.headers['content-length'];
-  if (contentLength && +contentLength > 0) {
-    const buffers = [];
-    for await (const chunk of req) {
-      buffers.push(chunk);
-    }
+  const buffers = [];
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
 
-    return JSON.parse(Buffer.concat(buffers).toString() ?? '');
-  } else return null;
+  return JSON.parse(Buffer.concat(buffers).toString() ?? '');
 }
